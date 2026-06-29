5ccd05ba5488b7867485495162d6dda96c48d081
Clemens <if24b063@technikum-wien.at>
2026-06-29 19:33:31 +0200

    Home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { TourListComponent } from './components/tour-list/tour-list';
-import { TourLogListComponent } from './components/tour-log-list/tour-log-list';
 import { StatisticsComponent } from './components/statistics/statistics';
 import { ImportExportComponent } from './components/import-export/import-export';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/tours', pathMatch: 'full' },
     { path: 'tours', component: TourListComponent },
-    { path: 'logs', component: TourLogListComponent },
     { path: 'statistics', component: StatisticsComponent },
     { path: 'import-export', component: ImportExportComponent }
 ];
