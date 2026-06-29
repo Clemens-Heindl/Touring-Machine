1b96f53f9da975536398fcbaf00296a0b4b413cf
Benni <benjam.winkler@gmail.com>
2026-06-29 16:47:49 +0200

    replace map placeholder with leaflet component

--- frontend/src/app/components/tour-details/tour-details.ts
+++ frontend/src/app/components/tour-details/tour-details.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { TourStateService } from '../../services/tour-state.service';
+import { MapComponent } from '../map/map.component';
 
 
 @Component({
   selector: 'app-tour-details',
   standalone: true,
-  imports: [],
+  imports: [MapComponent],
   templateUrl: './tour-details.html',
   styleUrls: ['./tour-details.css']
 })
