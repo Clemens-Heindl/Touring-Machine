4c6e5225ba75e6eb960f033aa1ccd648822630db
Benni <benjam.winkler@gmail.com>
2026-07-01 10:19:43 +0200

    Remove committed cruft (node_modules, dead stubs), gitignore, de-track appsettings.Development.json, add placeholder config

--- frontend/src/app/components/tour-log-form/tour-log-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-tour-log-form',
-  imports: [],
-  templateUrl: './tour-log-form.html',
-  styleUrl: './tour-log-form.css',
-})
-export class TourLogForm {}
